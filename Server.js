7bc4bc1db395853abaaa0b1ebadf1ae9a1b3313a
Madhavan C <139116570+madhavan521@users.noreply.github.com>
2024-10-02 10:49:18 +0530

    Server.js

--- Server.js
+++ Server.js
@@ -22,7 +22,7 @@ const notificationrouter = require("./Route/NotificationRouter");
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:3000/', 
+    origin: 'http://localhost:3000', 
     credentials: true  
   }));
   app.use('/api/auth' , authrouter)
